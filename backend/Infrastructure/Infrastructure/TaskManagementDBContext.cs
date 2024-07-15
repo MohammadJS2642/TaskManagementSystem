25da9804dab5ad4b923f50a7b418b0729fb491f7
MohammadJS2642 <mohammadjs2642@gmail.com>
2024-07-15 16:23:32 +0330

    ef core package and db context added to infrastructure project

---
+++ backend/Infrastructure/Infrastructure/TaskManagementDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public class TaskManagementDBContext(DbContextOptions<TaskManagementDBContext> options)
+    : DbContext(options)
+{
+    public DbSet<UserTask> UserTasks { get; set; }
+    public DbSet<User> Users { get; set; }
+}
